8210c6be4c6f8ffcecd4b6a72cf298b3d456847f
Ktento <kento07210525mo@outlook.jp>
2024-11-23 19:11:42 +0900

    Supabaseへの接続を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export default function Home() {
+  async function Comments() {
+    const supabase = await createClient(supabaseUrl, supabaseKey);
+    const { data: comments } = await supabase.from("comments").select();
+    return <pre>{JSON.stringify(comments, null, 2)}</pre>;
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-    </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
   );
 }
