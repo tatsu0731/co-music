c0a6d532cd9276c2fbfa3df80b573a32d1dba315
Ktento <kento07210525mo@outlook.jp>
2024-11-24 13:44:25 +0900

    返信を取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,22 @@
 "use client";
-
 import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-async function fetchCommentsFromSupabase() {
-  const { data, error } = await supabase.from("comments").select("*");
-  if (error) {
-    console.error(error);
-    throw new Error("データの取得に失敗しました");
-  }
-  console.log(data);
-  return data;
-}
-async function fetchCommentFromSupabase() {
-  const { data, error } = await supabase.from("comments").select("*");
-  if (error) {
-    console.error(error);
-    throw new Error("データの取得に失敗しました");
-  }
-  console.log(data);
-  return data;
-}
-
 export default function Home() {
-  const [comments, setComments] = useState<any[]>([]); // 型を定義
+  async function Comments() {
+    const supabase = await createClient(supabaseUrl, supabaseKey);
+    const { data: comments } = await supabase.from("comments").select(`
+      *,
+      reply_comments (
+      *
+      )
+    `);
+    console.log(comments);
 
-  useEffect(() => {
-    async function loadComments() {
-      const fetchComments = await fetchCommentsFromSupabase();
-      setComments(fetchComments);
-    }
-    loadComments();
-  }, []);
+    return <pre>{JSON.stringify(comments, null, 2)}</pre>;
+  }
 
+  Comments();
   return (
     <>
       <header className="flex py-1 px-2 gap-2 my-2">
@@ -46,10 +26,7 @@ export default function Home() {
         </div>
       </header>
       <section title="sec-music" className="flex justify-center mb-4">
-        <audio controls>
-          <source src="/music.mp3" type="audio/mpeg" />
-          お使いのブラウザはaudioタグをサポートしていません。
-        </audio>
+        <div className="w-80 bg-slate-300 h-52 rounded-md text-white">再生</div>
       </section>
       <div className="grid items-center justify-items-center gap-16 mx-2">
         <section className="flex flex-col gap-y-4">
@@ -60,22 +37,19 @@ export default function Home() {
             <div className="py-1 px-3 border-2 border-blue-300 rounded-md">
               <p>
                 ここのライブアレンジめっちゃぶち上がった！！
-                <span className="text-blue-500">02:35</span>
+                <span className=" text-blue-500">02:35</span>
               </p>
             </div>
-            <p className="text-xs">12:00</p>
+            <p className=" text-xs">12:00</p>
           </section>
           <section title="state" className="flex gap-2">
             <div className="p-2 bg-blue-200 rounded-md text-white w-10 h-10">
               ゆ
             </div>
             <div className="py-1 px-3 border-2 border-blue-300 rounded-md">
-              {/* コメント一覧を表示 */}
-              {comments.map((comment: any) => (
-                <p key={comment.comment_id}>{comment.comment}</p>
-              ))}
+              <p>分かる！それな！会場揺れた！</p>
             </div>
-            <p className="text-xs">12:00</p>
+            <p className=" text-xs">12:00</p>
           </section>
         </section>
         <section className="bottom-0 fixed w-full mx-4">
