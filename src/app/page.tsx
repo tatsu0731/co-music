1e4cdb4366930760932aa6feb9d69a4941217b91
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2024-11-24 10:48:45 +0900

    add: get処理記述

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
+"use client"
+
 import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-export default function Home() {
-  async function Comments() {
-    const supabase = await createClient(supabaseUrl, supabaseKey);
-    const { data: comments } = await supabase.from("comments").select();
-    return <pre>{JSON.stringify(comments, null, 2)}</pre>;
+const supabase = await createClient(supabaseUrl, supabaseKey);
+
+async function fetchCommentsFromSupabase() {
+  const { data, error } = await supabase.from("comments").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("データの取得に失敗しました");
   }
+  console.log(data)
+  return data;
+}
+
+export default function Home() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    async function loadComments() {
+        const fetchComments = await fetchCommentsFromSupabase();
+        setComments(fetchComments);
+    }
+    loadComments();
+    console.log(comments)
+  }, []);
 
   return (
     <>
@@ -38,7 +59,7 @@ export default function Home() {
               ゆ
             </div>
             <div className="py-1 px-3 border-2 border-blue-300 rounded-md">
-              <p>分かる！それな！会場揺れた！</p>
+              <p>{comments}</p>
             </div>
             <p className=" text-xs">12:00</p>
           </section>
